f4297d3f069c478549fd86d229413936d809f67f
Guillaume Hivert <guillaume.hivert@outlook.com>
2015-10-14 12:39:39 +0200

    WIP

--- 4I900 - Complex/johnson.rb
+++ 4I900 - Complex/johnson.rb
@@ -1,39 +1,38 @@
 require 'ostruct'
 
-@poids = [ [35, 12, 25, 6, 76, 56, 54, 25],
-           [26, 15, 41, 15, 65, 68, 42, 32] ]
-@not_1 = @poids[0].length + 1
+@weight = [ [35, 12, 25, 6,  76, 56, 54, 25],
+            [26, 15, 41, 15, 65, 68, 42, 32] ]
 
-def smallest_poids
-  x = 0; y = 0; min = 1 << 31
-  (0...@poids.length).each do |i|
-    (0...@poids[0].length).each do |j|
-      next if @poids[i][j] == -1
-      next unless @poids[i][j] < min
-      min = @poids[i][j]
+# Find smallest weight in tabs.
+def smallest_weight
+  x = -1; y = -1; min = 1 << 31
+  (0...@weight.length).each do |i|
+    (0...@weight[0].length).each do |j|
+      next if @weight[i][j] == -1
+      next unless @weight[i][j] < min
+      min = @weight[i][j]
       x = i
       y = j
     end
   end
-  (0..1).each { |ind| @poids[ind][y] = -1 }
-  puts "#{x}, #{y}"
-  @not_1 -= 1
+  (0..1).each { |ind| @weight[ind][y] = -1 }
   OpenStruct.new(:i => x, :j => y)
 end
 
+# Apply Johnson algorithm.
 def johnson
-  g = Array.new(0); d = Array.new(0)
-  while @not_1 != 0
-    puts "#{@poids}"
-    min = smallest_poids
-    puts "#{min}"
+  g = []
+  d = []
+  loop do
+    min = smallest_weight
+    break if min.j == -1 && min.i == -1
     if min.i == 0
       g << min.j
     else
       d.unshift(min.j)
     end
   end
-  g + d
+  (g + d).map { |a| a + 1 }
 end
 
 puts "#{johnson}"
