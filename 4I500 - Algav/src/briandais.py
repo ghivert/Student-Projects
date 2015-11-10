9849e2bf6188db3658c4e8ce990b5d6b9d784b22
Guillaume Hivert <guillaume.hivert@outlook.com>
2015-11-10 15:13:17 +0100

    Merge and bugfixes

--- 4I500 - Algav/src/briandais.py
+++ 4I500 - Algav/src/briandais.py
@@ -17,11 +17,12 @@ class BriandaisTree(object):
     def __init__(self, word, key = None):
         self.key = key # Keep the key.
         self.sons = []
+        self.final = False
 
-        if len(word) == 0: # Determine if the letter is final.
-            self.final = True
+        if len(word) == 0:
+            if self.key != None:
+                self.final = True
         else:
-            self.final = True
             self.add_word(word.lower())
 
     # Add a word to the tree.
@@ -54,12 +55,12 @@ class BriandaisTree(object):
     def number_words(self, key = None):
         number = 0
         if self.sons == []:
-            if self.final == True and self.key != None: # On a leaf.
+            if self.final == True: # On a leaf.
                 number += 1
         else:
             for son in self.sons:
                 number += son.number_words(str(key) + str(self.key))
-            if self.final == True and self.key != None: # On a word.
+            if self.final == True: # On a word.
                 number += 1
         return number
 
@@ -68,17 +69,17 @@ class BriandaisTree(object):
         list = []
         def get_all(tree, word = ''):
             tree.sons.sort(key = lambda x: x.key)
-            if tree.sons == []:
-                if tree.final == True and tree.key != None:
+            if tree.sons == []: # On a leaf.
+                if tree.final == True and tree.key != None: # Word is final.
                     list.append(word + str(tree.key))
-            else:
+            else: # Everywhere else in the tree.
                 if tree.final == True and tree.key != None:
                     list.append(word + str(tree.key))
                 for son in tree.sons:
-                    if tree.key != None:
-                        get_all(son, word + str(tree.key))
-                    else:
+                    if tree.key == None: # On the root.
                         get_all(son, word)
+                    else: # Everywhere else in the tree.
+                        get_all(son, word + str(tree.key))
         get_all(self)
         return list
 
@@ -122,22 +123,22 @@ class BriandaisTree(object):
                         answer.append(pref + word) # Add the word cause it's final.
                     for son in tree.sons: # Recursively search every other words.
                         get_all(son, word[1:], pref + tree.key)
-        get_all(self, word)
+        get_all(self, word.lower())
         return answer
 
     # Delete a word from the tree.
     def suppress(self, word):
         if self.key == None: # On the root.
             for son in self.sons:
-                suppr = son.suppress(word)
+                suppr = son.suppress(word.lower())
                 if suppr == True:
                     self.sons.remove(son) # Suppression to keep compacity.
         else: # Everywhere else.
             if len(word) == 0: # Son of the word to suppress.
                 return False
-            if self.key == word[0]: # In the prefix.
+            if self.key == word[0].lower(): # In the prefix.
                 for son in self.sons:
-                    suppr = son.suppress(word[1:])
+                    suppr = son.suppress(word[1:].lower())
                     if suppr == True:
                         self.sons.remove(son) # Suppression to keep compacity.
                 if self.sons == []:
@@ -145,6 +146,13 @@ class BriandaisTree(object):
                 else: # Word have to be deleted, but other words depends on it.
                     self.final = False
                     return False
+
+    # Add all words from the tree into itself.
+    def merge(self, tree):
+        # The easy way...
+        words = tree.all_words()
+        for word in words:
+            self.add_word(word)
 
     # Add a string representation.
     def spaces(self, nb):
@@ -155,6 +163,44 @@ class BriandaisTree(object):
     def __repr__(self):
         return self.spaces(0)
 
+# Merge two trees into one.
+def merge(first, second):
+    # The complex way...
+
+    if first.key != second.key: # Can't append, but in case...
+        raise Exception("Not a normal situation...")
+
+    tree = BriandaisTree('') # Instantiate new tree.
+    tree.key = first.key # Keep the key.
+    tree.final = True if first.final == True or second.final == True else False
+    i = 0; j = 0
+    # Sort the sons.
+    first.sons.sort(key = lambda x: x.key)
+    second.sons.sort(key = lambda x: x.key)
+
+    # If one tree is empty, keep everything else.
+    if len(first.sons) == 0:
+        for son in second.sons:
+            tree.sons.append(son)
+        return tree
+    if len(second.sons) == 0:
+        for son in first.sons:
+            tree.sons.append(son)
+        return tree
+
+    # Merge the trees.
+    while i < len(first.sons) and j < len(second.sons):
+        if first.sons[i].key < second.sons[j].key:
+            tree.sons.append(first.sons[i])
+            i += 1
+        elif first.sons[i].key > second.sons[j].key:
+            tree.sons.append(second.sons[j])
+            j += 1
+        else:
+            tree.sons.append(merge(first.sons[i], second.sons[j]))
+            i += 1; j += 1
+    return tree
+
 # Functions to comply the specifications...
 def BriandaisTreeVide():
     return BriandaisTree('')
@@ -164,8 +210,14 @@ def Recherche(tree, word):
     return tree.contains(word)
 def ComptageMot(tree):
     return tree.number_words()
-def ComptageNil():
-    return 0
+def ComptageNil(tree):
+    if tree.is_empty:
+        return 1
+    else:
+        number = 0
+        for son in tree.sons:
+            number += ComptageNil(son)
+        return number
 def Hauteur(tree):
     return tree.height()
 def ProfondeurMoyenne(tree):
@@ -175,3 +227,5 @@ def Prefixe(tree, word):
 def Suppression(tree, word):
     tree.suppress(word)
     return tree
+def Fusion(arbre1, arbre2):
+    return merge(arbre1, arbre2)
