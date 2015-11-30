4bb6d57cf49f0de7d60cf516a3fdc26a942652df
Guillaume Hivert <guillaume.hivert@outlook.com>
2015-11-30 22:11:43 +0100

    Lecture finie ! (Mais pas assez robuste...)

--- NW411 - Mogpl/src/reading.py
+++ NW411 - Mogpl/src/reading.py
@@ -1,5 +1,5 @@
 class Maze(object):
-    """Maze"""
+    """Representent a Maze object. Contains an array representing the ground."""
     def __init__(self):
         self.maze = []
 
@@ -39,20 +39,30 @@ class Maze(object):
 class MazeReader(object):
     """Read a maze file and return an initialized Maze."""
     def read_maze(self, maze_file):
+        """Read a maze file (.maze), and return a list of all mazes of the file."""
         mazes = []
-        maze = Maze()
         file = open(maze_file, 'r')
-        size = list(map(lambda x: int(x), file.readline().split(' ')))
-        for i in range(size[0]):
-            line = list(map(lambda x: int(x), file.readline().split(' ')))
-            maze.maze.append(line)
-        directions = file.readline().split(' ')
-        for i in range(4):
-            directions[i] = int(directions[i])
-        maze.starting_point(directions[0:2])
-        maze.ending_point(directions[2:4])
-        maze.orientation(directions[4])
-        mazes.append(maze)
+        while True:
+            maze = Maze()
+            size = list(map(lambda x: int(x), file.readline().split(' ')))
+            if size[0] == 0 and size[1] == 0:
+                break
+            for i in range(size[0]):
+                line = list(map(lambda x: int(x), file.readline().split(' ')))
+                if len(line) != size[1]:
+                    raise Exception
+                maze.maze.append(line)
+            directions = file.readline().split(' ')
+            for i in range(4):
+                directions[i] = int(directions[i])
+            maze.starting_point(directions[0:2])
+            maze.ending_point(directions[2:4])
+            maze.orientation(directions[4])
+            mazes.append(maze)
+        file.close()
+        return mazes
 
 mr = MazeReader()
-mr.read_maze('../instances/small.maze').print_maze()
+for i in mr.read_maze('../instances/small.maze'):
+    i.print_maze()
+    print('')
