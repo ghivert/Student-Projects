a42340a84f5520d98e38fbc2676622f9472320a7
Guillaume Hivert <guillaume.hivert@outlook.com>
2016-01-28 23:46:48 +0100

    C implem.

--- 4I504 - Compilation/src/reading.c
+++ 4I504 - Compilation/src/reading.c
@@ -6,9 +6,17 @@ Code bread_file(char *filename) {
   FILE *fh = fopen(filename, "rb");
   fseek(fh, 0, SEEK_END);
   rcode.size = ftell(fh);
-  rcode.code = malloc(rcode.size + 1);
+  rcode.code = malloc(rcode.size);
   fseek(fh, 0, SEEK_SET);
-  fread(rcode.code, 4, rcode.size, fh);
+  rcode.code[0] = 0;
+  
+  fread(rcode.code, 4, rcode.size / 4, fh);
+  for (int i = 0; i < rcode.size / 4; i++) {
+    rcode.code[i] = ((rcode.code[i] >> 24) & 0xff) | // move byte 3 to byte 0
+      ((rcode.code[i] << 8) & 0xff0000) | // move byte 1 to byte 2
+      ((rcode.code[i] >> 8) & 0xff00) | // move byte 2 to byte 1
+      ((rcode.code[i] << 24) & 0xff000000); // byte 0 to byte 3
+  }
   fclose(fh);
   
   return rcode;
--- 4I504 - Compilation/src/universal_machine.c
+++ 4I504 - Compilation/src/universal_machine.c
@@ -1,13 +1,16 @@
 #include "universal_machine.h"
 
+// Opcode.
+#define get_opc(num) (num >> 28) & 0b1111
+
 // Computed GOTO.
-#define next()							\
-  do {								\
-    val = arrays[0].code[pc];					\
-    a = (val >> 6) & 0b111;					\
-    b = (val >> 3) & 0b111;					\
-    c = (val >> 0) & 0b111;					\
-    goto *opcodes[get_opc(arrays[0].code[pc++])];     		\
+#define next()						\
+  do {							\
+    val = arrays[0].code[pc];				\
+    a = (val >> 6) & 0b111;				\
+    b = (val >> 3) & 0b111;				\
+    c = (val >> 0) & 0b111;				\
+    goto *opcodes[get_opc(arrays[0].code[pc++])];	\
   } while(0)
 #define stop() goto end
 
@@ -15,73 +18,80 @@
 unsigned int reg[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
 // Get the opcode.
-unsigned int get_opc(unsigned int num) {
-  return (num >> 28) & 0b1111;
-}
+
+#define debug(opcode) ;//printf("%s, pc = %x,\n\ta = %d, b = %d, c = %d \n", opcode, pc, reg[a], reg[b], reg[c])
 
 // LET'S GO !
 void exec_vm(Code program) {
-  unsigned int size = 10;
+  unsigned int size = 10, arr = 1;
   Code *arrays = malloc(sizeof *arrays * size);
   for (unsigned int i = 0; i < size; i++)
     arrays[i].size = -1;
   arrays[0] = program;
-  
+
   static void* opcodes[] = {
     &&cmv,     &&aind,    &&aamd, &&add,
     &&mul,     &&div,     &&nand, &&halt,
     &&alloc,   &&abandon, &&out,  &&in,
     &&lprog,   &&orth
   };
-  
+
   unsigned int pc = 0;
   unsigned int val = 0;
   unsigned int a, b, c;
-  
+
   next();
-  
+
  cmv:
+  debug("cmv");
   if (reg[c] != 0)
     reg[a] = reg[b];
   next();
-  
+
  aind:
-  reg[a] = arrays[b].code[reg[c]];
+  debug("aind");
+  printf("%d\n", reg[b]);
+  reg[a] = arrays[reg[b]].code[reg[c]];
   next();
-  
+
  aamd:
-  arrays[a].code[reg[b]] = reg[c];
+  debug("aamd");
+  arrays[reg[a]].code[reg[b]] = reg[c];
   next();
-  
+
  add:
+  debug("add");
   reg[a] = (reg[b] + reg[c]) % 4294967296; // 2**32.
   next();
-  
+
  mul:
+  debug("mul");
   reg[a] = (reg[b] * reg[c]) % 4294967296;
   next();
-  
+
  div:
+  debug("div");
   if (reg[c] != 0)
+    //printf("%ld %ld %08x\n", reg[a], reg[b], (unsigned int) reg[c]);
+    //printf("%d\n", reg[b] / reg[c]);
     reg[a] = ((unsigned int) reg[b] / (unsigned int) reg[c]);
   next();
-  
+
  nand:
+  debug("nand");
   reg[a] = ~(reg[b] & reg[c]);
   next();
-  
+
  halt:
+  debug("halt");
   stop();
-  
+
  alloc:
-  if (size > reg[b] && arrays[reg[b]].size != -1) {
-    fprintf(stderr, "Array collision.\n");
-    exit(EXIT_FAILURE);
-  }
-  
-  if (size < reg[b]) {
+  debug("alloc");
+
+  if (size < arr) {
     Code *arrays_p;
-    size = reg[b] + 1;
+    size = size * 2;
     if ((arrays_p = realloc(arrays, size)) != NULL) {
       free(arrays);
       arrays = arrays_p;
@@ -90,30 +100,34 @@ void exec_vm(Code program) {
       exit(EXIT_FAILURE);
     }
   }
-  
+
   Code new;
   new.code = malloc(sizeof *(new.code) * reg[c]);
   new.size = reg[c];
   for (unsigned int i = 0; i < reg[c]; i++)
     new.code[i] = 0;
-  arrays[reg[b]] = new;
+  arrays[size] = new;
+  reg[b] = size++;
 
   next();
-  
+
  abandon:
+  debug("abandon");
   free(arrays[reg[c]].code);
   arrays[reg[c]].size = -1;
   next();
-  
+
  out:
+  debug("out");
   if (reg[c] > 255) {
     fprintf(stderr, "Output : bad number.\n");
     exit(EXIT_FAILURE);
   }
-  printf("%c\n", (unsigned char) reg[c]);
+  printf("%c", (unsigned char) reg[c]);
   next();
-  
- in: 
+
+ in:
+  printf("in");
   {
     char input;
     if ((input = getc(stdin)) == EOF)
@@ -123,20 +137,27 @@ void exec_vm(Code program) {
     next();
   }
  lprog:
-  free(arrays[0].code);
-
-  arrays[0].size = arrays[reg[b]].size;
-  arrays[0].code = malloc(sizeof *(new.code) * new.size);
-  for (int i = 0; i < new.size; i++)
-    arrays[0].code[i] = arrays[reg[b]].code[i];
+  debug("lprog");
+  if (reg[b] != 0) {
+    Code temp;
+    temp.size = arrays[reg[b]].size;
+    temp.code = malloc(sizeof *(temp.code) * temp.size);
+    //printf("Ici\n");
+    for (int i = 0; i < temp.size; i++)
+      temp.code[i] = arrays[reg[b]].code[i];
+    free(arrays[0].code);
+    //printf("Ou la.\n");
+    arrays[0] = temp;
+  }
   pc = reg[c];
 
   next();
-  
+
  orth:
+  debug("orth");
   reg[(val >> 25) & 0b111] = val & 0x1FFFFFF;
   next();
-  
+
  end:
   return;
 }
