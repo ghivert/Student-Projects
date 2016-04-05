931edc369c4d208fdf329277fee8aeb4c223bf00
Dada <darius.mercadier@gmail.com>
2016-04-05 06:09:43 +0200

    fixing server

--- 4I507 - PC2R/lib/server/server.go
+++ 4I507 - PC2R/lib/server/server.go
@@ -102,7 +102,7 @@ func (h *hub) run() {
 func game(h *hub, session chan []byte, start chan int) {
 	max_round := 500
 	reflexion_time := 30 // seconds
-	bid_time := 30 // seconds
+	bid_time := 2 // seconds
 	res_time := 30
 	max_bid := 1000000000 // 1000000000 is just a random 'big' number
 
@@ -118,107 +118,107 @@ func game(h *hub, session chan []byte, start chan int) {
          - a Wall array (Wall = coordonates + orientation) to check if solutions are valids.
          - a string array helps building those 2 representations
     */
-  for {
-	board_walls := []string{"(4,1,D)","(6,3,D)","(6,3,B)","(3,5,H)","(3,5,D)",
-		"(1,5,B)","(8,6,G)","(8,6,B)","(2,7,H)","(2,7,G)","(8,8,G)","(8,8,H)","(10,1,D)",
-		"(14,2,D)","(14,2,B)","(16,2,B)","(10,4,G)","(10,4,H)","(15,5,H)","(15,5,D)",
-		"(13,7,G)","(13,7,B)","(9,8,H)","(9,8,D)","(8,9,G)","(8,9,B)","(5,10,G)",
-		"(5,10,B)","(1,11,B)","(7,11,G)","(7,11,H)","(8,13,H)","(8,13,D)","(2,14,H)",
-		"(2,14,D)","(4,15,B)","(4,15,D)","(5,16,D)","(9,9,D)","(9,9,B)","(14,10,G)",
-		"(14,10,B)","(10,12,B)","(10,12,D)","(16,12,B)","(15,14,H)","(15,14,D)",
-		"(11,15,G)","(11,15,H)","(12,16,D)"}
-	board := strings.Join(board_walls, "")
+	for {
+		board_walls := []string{"(4,1,D)","(6,3,D)","(6,3,B)","(3,5,H)","(3,5,D)",
+			"(1,5,B)","(8,6,G)","(8,6,B)","(2,7,H)","(2,7,G)","(8,8,G)","(8,8,H)","(10,1,D)",
+			"(14,2,D)","(14,2,B)","(16,2,B)","(10,4,G)","(10,4,H)","(15,5,H)","(15,5,D)",
+			"(13,7,G)","(13,7,B)","(9,8,H)","(9,8,D)","(8,9,G)","(8,9,B)","(5,10,G)",
+			"(5,10,B)","(1,11,B)","(7,11,G)","(7,11,H)","(8,13,H)","(8,13,D)","(2,14,H)",
+			"(2,14,D)","(4,15,B)","(4,15,D)","(5,16,D)","(9,9,D)","(9,9,B)","(14,10,G)",
+			"(14,10,B)","(10,12,B)","(10,12,D)","(16,12,B)","(15,14,H)","(15,14,D)",
+			"(11,15,G)","(11,15,H)","(12,16,D)"}
+		board := strings.Join(board_walls, "")
 
-	board_array := make([]Wall, len(board_walls))
-	for i, val := range board_walls {
-		vals := strings.Split(val[1:len(val)-1], ",")
-		x, _ := strconv.Atoi(vals[0])
-		y, _ := strconv.Atoi(vals[1])
-		board_array[i] = Wall{x: x, y: y, orient: vals[2]}
-	}
-
-	// Notifying the start of the game to the clients.
-	session     <- []byte("SESSION/" + board + "/\n")
-	h.broadcast <- []byte("SESSION/" + board + "/\n")
-	// fmt.Fprintln(os.Stderr, "  ~~~ board sent")
-
-	// the game loop.
-	for round_nb := 1; round_nb <= max_round; round_nb++ {
-
-		// If there is only 0 or 1 client, exiting.
-		if len(h.clients) < 1 {
-			// fmt.Fprintln(os.Stderr, "/!\\ Not enough clients, exiting")
-			break
+		board_array := make([]Wall, len(board_walls))
+		for i, val := range board_walls {
+			vals := strings.Split(val[1:len(val)-1], ",")
+			x, _ := strconv.Atoi(vals[0])
+			y, _ := strconv.Atoi(vals[1])
+			board_array[i] = Wall{x: x, y: y, orient: vals[2]}
 		}
 
-		// PHASE DE REFLEXION
+		// Notifying the start of the game to the clients.
+		session     <- []byte("SESSION/" + board + "/\n")
+		h.broadcast <- []byte("SESSION/" + board + "/\n")
+		// fmt.Fprintln(os.Stderr, "  ~~~ board sent")
 
-		/* Generating the puzzle, and generating the string that will be sended to the clients.
+		// the game loop.
+		for round_nb := 1; round_nb <= max_round; round_nb++ {
+
+			// If there is only 0 or 1 client, exiting.
+			if len(h.clients) < 1 {
+				// fmt.Fprintln(os.Stderr, "/!\\ Not enough clients, exiting")
+				break
+			}
+
+			// PHASE DE REFLEXION
+
+			/* Generating the puzzle, and generating the string that will be sended to the clients.
 
            At the same time, we fill the bids hashmap (to avoid iterating one more time through
            the list of players.
            bids hashmap is used to store the bids of the clients and to know which players are
            playing this round.
         */
-		enigme := gen_start()
-		buffer := []byte("TOUR/" + enigme + "/" + strconv.Itoa(round_nb))
-		bids := make(map[string]int)
-		for p := range h.clients {
-			name := p.name
-			score := p.score
-			buffer = append(buffer, "(" + name + "," + strconv.Itoa(score) + ")"...)
-			bids[name] = max_bid
-		}
-		buffer = append(buffer, "/\n"...)
-		h.broadcast <- buffer
-		fmt.Fprintln(os.Stderr, "  ~~~ puzzle sent")
+			enigme := gen_start()
+			buffer := []byte("TOUR/" + enigme + "/" + strconv.Itoa(round_nb))
+			bids := make(map[string]int)
+			for p := range h.clients {
+				name := p.name
+				score := p.score
+				buffer = append(buffer, "(" + name + "," + strconv.Itoa(score) + ")"...)
+				bids[name] = max_bid
+			}
+			buffer = append(buffer, "/\n"...)
+			h.broadcast <- buffer
+			fmt.Fprintln(os.Stderr, "  ~~~ puzzle sent")
 
-		// a timeout to limit the time of this phase.
-		timeout_chan := make(chan int, 1)
-		timeout(timeout_chan, reflexion_time)
+			// a timeout to limit the time of this phase.
+			timeout_chan := make(chan int, 1)
+			timeout(timeout_chan, reflexion_time)
 
-		fmt.Fprintln(os.Stderr, "*** Reflexion phase")
-		var min int = max_bid
-		var minName string
+			fmt.Fprintln(os.Stderr, "*** Reflexion phase")
+			var min int = max_bid
+			var minName string
 
 		reflexion_phase:
-		for {
-			select {
-		    case <- timeout_chan:
-			    break reflexion_phase
-		    case action := <- h.action:
-			    value := action.value
-			    user  := action.user
-			    s := strings.Split(string(value[:]), "/")
-			    if nb, err := strconv.Atoi(s[2]);
-				s[0] == "SOLUTION" && s[1] == user.name && err == nil {
-					if _, ok := bids[user.name]; ok {
-						min = nb
-						minName = user.name
-						bids[user.name] = nb
-						user.send <- []byte("TUASTROUVE/\n")
-						h.broadcast <-
-							[]byte("ILATROUVE/" + user.name + "/" + s[2] + "/\n")
-						break reflexion_phase
+			for {
+				select {
+				case <- timeout_chan:
+					break reflexion_phase
+				case action := <- h.action:
+					value := action.value
+					user  := action.user
+					s := strings.Split(string(value[:]), "/")
+					if nb, err := strconv.Atoi(s[2]);
+					s[0] == "SOLUTION" && s[1] == user.name && err == nil {
+						if _, ok := bids[user.name]; ok {
+							min = nb
+							minName = user.name
+							bids[user.name] = nb
+							user.send <- []byte("TUASTROUVE/\n")
+							h.broadcast <-
+								[]byte("ILATROUVE/" + user.name + "/" + s[2] + "/\n")
+							break reflexion_phase
+						} else {
+							fmt.Fprintln(os.Stderr, "/!\\ Not allowed client : ", user.name)
+						}
 					} else {
-						fmt.Fprintln(os.Stderr, "/!\\ Not allowed client : ", user.name)
+						fmt.Fprintln(os.Stderr, "/!\\ Wrong request: ", string(value[:]))
 					}
-				} else {
-					fmt.Fprintln(os.Stderr, "/!\\ Wrong request: ", string(value[:]))
-		    	}
+				}
 			}
-		}
+			
+			h.broadcast <- []byte("FINREFLEXION/\n")
+			fmt.Fprintln(os.Stderr, "*** Reflexion phase over. Min = ", min)
 
-		h.broadcast <- []byte("FINREFLEXION/\n")
-		fmt.Fprintln(os.Stderr, "*** Reflexion phase over. Min = ", min)
-
-		// ENCHERES
-		timeout_chan = make(chan int)
-		timeout(timeout_chan, bid_time)
+			// ENCHERES
+			timeout_chan = make(chan int)
+			timeout(timeout_chan, bid_time)
 
 		encheres_loop:
-		for {
-			select {
+			for {
+				select {
 				case <- timeout_chan:
 				    for p := range bids {
 						if bids[p] < min {
@@ -230,7 +230,7 @@ func game(h *hub, session chan []byte, start chan int) {
 						h.broadcast <- []byte("FINENCHERE/null/null/\n")
 					} else {
 						h.broadcast <- []byte("FINENCHERE/" + minName +
-							                  "/" + strconv.Itoa(min) + "/\n")
+							"/" + strconv.Itoa(min) + "/\n")
 					}
 				    break encheres_loop
 				case action := <- h.action:
@@ -249,7 +249,7 @@ func game(h *hub, session chan []byte, start chan int) {
 								bids[user.name] = nb
 								user.send <- []byte("VALIDATION/\n")
 								h.broadcast <- []byte("NOUVELLEENCHERE/" + user.name + "/" +
-									                  strconv.Itoa(nb) + "/\n")
+									strconv.Itoa(nb) + "/\n")
 								if nb < min {
 									min = nb
 									minName = user.name
@@ -259,23 +259,23 @@ func game(h *hub, session chan []byte, start chan int) {
 							}
 						}
 					}
+				}
 			}
-		}
-		fmt.Fprintln(os.Stderr, "*** Encheres phase over. Min = ", min)
+			fmt.Fprintln(os.Stderr, "*** Encheres phase over. Min = ", min)
 
-		// RESOLUTION
-		res_loop: for {
-			if min == max_bid {
-				h.broadcast <- []byte("FINRESO/\n")
-				break
-			}
+			// RESOLUTION
+			res_loop: for {
+				if min == max_bid {
+					h.broadcast <- []byte("FINRESO/\n")
+					break
+				}
 
-			delete(bids, minName)
+				delete(bids, minName)
 
-			var nextMsg string
-			timeout_chan := make(chan int, 1)
-			timeout(timeout_chan, res_time)
-			select {
+				var nextMsg string
+				timeout_chan := make(chan int, 1)
+				timeout(timeout_chan, res_time)
+				select {
 				case <- timeout_chan:
 				    nextMsg = "TROPLONG/"
 				case action := <- h.action:
@@ -292,38 +292,38 @@ func game(h *hub, session chan []byte, start chan int) {
 							nextMsg = "MAUVAISE/"
 						}
 					}
-			}
-
-			var min_loc int = max_bid
-			var minName_loc string = ""
-			for p := range bids {
-				if bids[p] < min_loc {
-					min_loc = bids[p]
-					minName_loc = p
 				}
+
+				var min_loc int = max_bid
+				var minName_loc = "null"
+				for p := range bids {
+					if bids[p] < min_loc {
+						min_loc = bids[p]
+						minName_loc = p
+					}
+				}
+				min = min_loc
+				minName = minName_loc
+				h.broadcast <- []byte(nextMsg + minName + "/\n")
 			}
-			min = min_loc
-			minName = minName_loc
-			h.broadcast <- []byte(nextMsg + minName + "/\n")
+			fmt.Fprintln(os.Stderr, "Tour terminé.")
 		}
-		fmt.Fprintln(os.Stderr, "Tour terminé.")
+		buffer := []byte("VAINQUEUR/" + strconv.Itoa(max_round))
+		for p := range h.clients {
+			name := p.name
+			score := p.score
+			buffer = append(buffer, "(" + name + "," + strconv.Itoa(score) + ")"...)
+		}
+		buffer = append(buffer, "/\n"...)
+		h.broadcast <- buffer
 	}
-	buffer := []byte("VAINQUEUR/" + strconv.Itoa(max_round))
-	for p := range h.clients {
-		name := p.name
-		score := p.score
-		buffer = append(buffer, "(" + name + "," + strconv.Itoa(score) + ")"...)
-	}
-	buffer = append(buffer, "/\n"...)
-	h.broadcast <- buffer
-}
 	os.Exit(1)
 
 }
 
 func gen_start() string {
 	target_l := [...]string{"6,3","14,2","10,4","3,5","15,5","8,6","2,7","13,7",
-		        "4,10","14,10","7,11","10,12","8,13","2,14","15,14","4,15","11,15"}
+		"4,10","14,10","7,11","10,12","8,13","2,14","15,14","4,15","11,15"}
 	colors := []string{"B", "V", "R", "J"}
 
 	res := []string{"", "", "", "", "", ""}
@@ -344,7 +344,7 @@ func gen_start() string {
 	}
 
 select_target:
-	target := target_l[rand.Intn(len(target_l)) - 1]
+	target := target_l[rand.Intn(len(target_l)-1)]
 	for j := 0; j < 4; j++ {
 		if res[j] == target {
 			goto select_target
@@ -366,7 +366,6 @@ func check_solution(board []Wall, enigme string, sol string, nb_coups int) bool 
 	}
 	positions := strings.Split(enigme, ",")
 
-check_loop:
 	for len(sol) > 0 {
 
 		robot, ok := corres[sol[:1]]
@@ -376,136 +375,106 @@ check_loop:
 		}
 		dir   := sol[1:2]
 		sol    = sol[2:]
+
+
 		x, _ := strconv.Atoi(positions[robot*2])
 		y, _ := strconv.Atoi(positions[robot*2+1])
+		
 		switch dir {
-			case "H":
-			    for y > 1 {
-					for _, wall := range board {
-						if wall.x == x && wall.y == y && wall.orient == "H" {
-							for i := 0; i < 4; i++ {
-								if i != robot {
-									x2, _ := strconv.Atoi(positions[robot*2])
-									y2, _ := strconv.Atoi(positions[robot*2+1])
-									if x2 == x && y2 == y-1 {
-										continue check_loop
-									}
-								}
+		case "H":
+			for y > 1 {
+				for _, wall := range board {
+					if wall.x == x && wall.y == y && wall.orient == "H" {
+						goto update_co
+					}
+					if wall.x == x && wall.y == y-1 && wall.orient == "B" {
+						goto update_co
+					}
+					for i := 0; i < 4; i++ {
+						if i != robot {
+							x2, _ := strconv.Atoi(positions[i*2])
+							y2, _ := strconv.Atoi(positions[i*2+1])
+							if x2 == x && y2 == y-1 {
+								goto update_co
 							}
-							goto update_coord
 						}
-						if wall.x == x && wall.y == y-1 && wall.orient == "B" {
-							for i := 0; i < 4; i++ {
-								if i != robot {
-									x2, _ := strconv.Atoi(positions[robot*2])
-									y2, _ := strconv.Atoi(positions[robot*2+1])
-									if x2 == x && y2 == y-1 {
-										continue check_loop
-									}
-								}
-							}
-							goto update_coord
-						}
-						y--
 					}
 				}
-			case "B":
-			    for y < 16 {
-					for _, wall := range board {
-						if wall.x == x && wall.y == y && wall.orient == "B" {
-							for i := 0; i < 4; i++ {
-								if i != robot {
-									x2, _ := strconv.Atoi(positions[robot*2])
-									y2, _ := strconv.Atoi(positions[robot*2+1])
-									if x2 == x && y2 == y+1 {
-										continue check_loop
-									}
-								}
+				y--
+			}
+		case "B":
+			for y < 16 {
+				for _, wall := range board {
+					if wall.x == x && wall.y == y && wall.orient == "B" {
+						goto update_co
+					}
+					if wall.x == x && wall.y == y+1 && wall.orient == "H" {
+						goto update_co
+					}
+					for i := 0; i < 4; i++ {
+						if i != robot {
+							x2, _ := strconv.Atoi(positions[i*2])
+							y2, _ := strconv.Atoi(positions[i*2+1])
+							if x2 == x && y2 == y+1 {
+								goto update_co
 							}
-							goto update_coord
 						}
-						if wall.x == x && wall.y == y+1 && wall.orient == "H" {
-							for i := 0; i < 4; i++ {
-								if i != robot {
-									x2, _ := strconv.Atoi(positions[robot*2])
-									y2, _ := strconv.Atoi(positions[robot*2+1])
-									if x2 == x && y2 == y+1 {
-										continue check_loop
-									}
-								}
-							}
-							goto update_coord
-						}
-						y++
 					}
 				}
-			case "G":
-			    for x > 1 {
-					for _, wall := range board {
-						if wall.y == y && wall.x == x && wall.orient == "G" {
-							for i := 0; i < 4; i++ {
-								if i != robot {
-									x2, _ := strconv.Atoi(positions[robot*2])
-									y2, _ := strconv.Atoi(positions[robot*2+1])
-									if x2 == x-1 && y2 == y {
-										continue check_loop
-									}
-								}
+				y++
+			}
+		case "G":
+			for x > 1 {
+				for _, wall := range board {
+					if wall.y == y && wall.x == x && wall.orient == "G" {
+						goto update_co
+					}
+					if wall.y == y && wall.x == x-1 && wall.orient == "D" {
+						goto update_co
+					}
+					for i := 0; i < 4; i++ {
+						if i != robot {
+							x2, _ := strconv.Atoi(positions[i*2])
+							y2, _ := strconv.Atoi(positions[i*2+1])
+							if x2 == x-1 && y2 == y {
+								goto update_co
 							}
-							goto update_coord
 						}
-						if wall.y == y && wall.x == x-1 && wall.orient == "D" {
-							for i := 0; i < 4; i++ {
-								if i != robot {
-									x2, _ := strconv.Atoi(positions[robot*2])
-									y2, _ := strconv.Atoi(positions[robot*2+1])
-									if x2 == x-1 && y2 == y {
-										continue check_loop
-									}
-								}
-							}
-							goto update_coord
-						}
-						x--
 					}
 				}
-			case "D":
-			    for x < 16 {
-					for _, wall := range board {
-						if wall.y == y && wall.x == x && wall.orient == "D" {
-							for i := 0; i < 4; i++ {
-								if i != robot {
-									x2, _ := strconv.Atoi(positions[robot*2])
-									y2, _ := strconv.Atoi(positions[robot*2+1])
-									if x2 == x+1 && y2 == y {
-										continue check_loop
-									}
-								}
+				x--
+			}
+		case "D":
+			for x < 16 {
+				for _, wall := range board {
+					if wall.y == y && wall.x == x && wall.orient == "D" {
+						goto update_co
+					}
+					if wall.y == y && wall.x == x+1 && wall.orient == "G" {
+						goto update_co
+					}
+					for i := 0; i < 4; i++ {
+						if i != robot {
+							x2, _ := strconv.Atoi(positions[i*2])
+							y2, _ := strconv.Atoi(positions[i*2+1])
+							if x2 == x+1 && y2 == y {
+								goto update_co
 							}
-							goto update_coord
 						}
-						if wall.y == y && wall.x == x+1 && wall.orient == "G" {
-							for i := 0; i < 4; i++ {
-								if i != robot {
-									x2, _ := strconv.Atoi(positions[robot*2])
-									y2, _ := strconv.Atoi(positions[robot*2+1])
-									if x2 == x+1 && y2 == y {
-										continue check_loop
-									}
-								}
-							}
-							goto update_coord
-						}
-						x++
 					}
 				}
-			default:
-			    fmt.Fprintln(os.Stderr, "Direction inconnue : ", dir)
+				x++
+			}
+		default:
+			log.Fatal("Direction inconnue : ", dir)
 		}
-		continue check_loop
-	update_coord:
+	update_co:
 		positions[robot*2]   = strconv.Itoa(x)
 		positions[robot*2+1] = strconv.Itoa(y)
+	}
+
+	for i := 0; i < 4; i++ {
+		fmt.Fprintln(os.Stderr, i, ":", positions[i*2], ",", positions[i*2+1])
 	}
 
 	robot := corres[positions[10]]
@@ -568,6 +537,7 @@ func client(c net.Conn, h *hub) {
 
 	go func() {
 		for message := range co.send {
+			fmt.Fprintln(os.Stderr, "  Sending", s[1], ":", string(message[:len(message)-1]))
 			if _, err := c.Write(message); err != nil {
 				fmt.Fprintln(os.Stderr, "   - Writing error, gonna close client " + s[1] + "...")
 				break
@@ -582,8 +552,8 @@ func client(c net.Conn, h *hub) {
 			fmt.Fprintln(os.Stderr, "   - Reading error, gonna close client " + s[1] + "...")
 			return
 		}
+		fmt.Fprintln(os.Stderr, "  Reveived: ", string(message[:len(message)-1]))
 		go func () {
-			fmt.Fprintln(os.Stderr, string(message[:]))
 			s := strings.Split(string(message[:]), "/")
 			switch s[0] {
 	    	case "SORT":
